28be3b88fc185b047c41bd013292505e8a4ddd63
Gilgamer24 <gilgouper247@gmail.com>
2026-02-11 21:29:17 +0100

    Initialize server with Socket.IO for player management

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let players = {};
+
+io.on('connection', (socket) => {
+    players[socket.id] = { x: 0, z: 0, id: socket.id, color: Math.random() * 0xffffff };
+    socket.emit('currentPlayers', players);
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('playerMovement', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].z = data.z;
+            socket.broadcast.emit('playerMoved', players[socket.id]);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+});
+
+server.listen(process.env.PORT || 10000);
