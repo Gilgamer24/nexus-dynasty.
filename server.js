4ce463cdb0dfea26974a9cd6765cafe84aeac60b
Gilgamer24 <gilgouper247@gmail.com>
2026-02-12 17:54:47 +0100

    Enhance user login and worker purchase functionality

--- server.js
+++ server.js
@@ -5,38 +5,39 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-let db = { 
-    users: {}, 
-    buildings: [] // Stocke les constructions de tout le monde
-};
+let db = { users: {}, buildings: [] };
 
 io.on('connection', (socket) => {
     socket.on('login', (data) => {
         const { pseudo, mdp } = data;
         if (!db.users[pseudo]) {
-            db.users[pseudo] = { pseudo, mdp, gold: 1000, food: 100, hp: 100, zone: 'farm' };
+            db.users[pseudo] = { 
+                pseudo, mdp, gold: 200, food: 100, hp: 100, 
+                workers: 0, lastUpdate: Date.now() 
+            };
         } else if (db.users[pseudo].mdp !== mdp) {
-            return socket.emit('authError', "MDP Incorrect");
+            return socket.emit('authError', "Mauvais mot de passe");
         }
         socket.userId = pseudo;
         socket.emit('authSuccess', { user: db.users[pseudo], buildings: db.buildings });
     });
 
-    socket.on('build', (bData) => {
-        if (socket.userId && db.users[socket.userId].gold >= bData.cost) {
-            db.users[socket.userId].gold -= bData.cost;
-            const newBuilding = { ...bData, id: Date.now(), owner: socket.userId };
-            db.buildings.push(newBuilding);
-            io.emit('newBuilding', newBuilding);
-            socket.emit('updateGold', db.users[socket.userId].gold);
+    // Production automatique des mineurs
+    socket.on('buyWorker', () => {
+        let u = db.users[socket.userId];
+        if (u && u.gold >= 100) {
+            u.gold -= 100;
+            u.workers += 1;
+            socket.emit('updateStats', u);
         }
     });
 
     socket.on('requestRespawn', () => {
         if(socket.userId) {
-            db.users[socket.userId].gold = Math.floor(db.users[socket.userId].gold * 0.6);
-            db.users[socket.userId].hp = 100;
-            socket.emit('respawnDone', db.users[socket.userId]);
+            let u = db.users[socket.userId];
+            u.gold = Math.floor(u.gold * 0.6);
+            u.hp = 100; u.food = 100;
+            socket.emit('respawnDone', u);
         }
     });
 
@@ -45,4 +46,4 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => console.log("Nexus Server Ready"));
+http.listen(3000, () => console.log("Nexus Economy Engine Ready"));
