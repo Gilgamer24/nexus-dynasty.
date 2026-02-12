35c1ad6dd70957dc66ba43670ef6d0ec28aa83e4
Gilgamer24 <gilgouper247@gmail.com>
2026-02-12 16:17:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-// Dans ton fichier server.js (partie Socket.io)
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+let players = {};
+
 io.on('connection', (socket) => {
-    console.log('Un joueur est connecté : ' + socket.id);
-    
-    // Création du joueur avec une couleur aléatoire
+    // Créer un nouveau joueur
     players[socket.id] = {
         x: 0, y: 0, z: 0, ry: 0,
         id: socket.id,
-        color: Math.floor(Math.random() * 0xFFFFFF)
+        color: Math.floor(Math.random() * 0xffffff)
     };
 
-    // Envoyer la liste actuelle à celui qui vient de se connecter
+    // Envoyer la liste à l'arrivant
     socket.emit('currentPlayers', players);
-
-    // Prévenir les autres qu'un nouveau arrive
+    // Prévenir les autres
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    // Relayer les mouvements à TOUT LE MONDE
-    socket.on('playerMovement', (movementData) => {
+    socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            players[socket.id].z = movementData.z;
-            players[socket.id].ry = movementData.ry;
-            // On renvoie l'info aux autres
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].z = data.z;
+            players[socket.id].ry = data.ry;
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -32,3 +35,5 @@ io.on('connection', (socket) => {
         io.emit('playerDisconnected', socket.id);
     });
 });
+
+http.listen(3000, () => { console.log('Serveur sur port 3000'); });
