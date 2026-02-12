84d7503578a1806b8e1959242b9d3e80b52b2fa6
Gilgamer24 <gilgouper247@gmail.com>
2026-02-12 16:59:36 +0100

    Refactor player initialization and movement handling

--- server.js
+++ server.js
@@ -2,64 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
 app.use(express.static(__dirname));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Nouvelle Dynastie : ' + socket.id);
-
-    // Initialisation du compte joueur
-    players[socket.id] = {
-        id: socket.id,
-        name: "Maison " + socket.id.substring(0, 4),
-        x: Math.random() * 20 - 10,
-        y: 0,
-        z: Math.random() * 20 - 10,
-        ry: 0,
-        color: Math.floor(Math.random() * 0xffffff),
-        resources: { gold: 100, metal: 50, food: 50 },
-        buildings: { mine: 1, farm: 1, barracks: 0 },
-        army: 0
-    };
-
-    socket.emit('init', players[socket.id]);
-    io.emit('currentPlayers', players);
-
-    // Boucle de Simulation (Production toutes les 2 secondes)
-    const gameLoop = setInterval(() => {
-        if (players[socket.id]) {
-            let p = players[socket.id];
-            p.resources.gold += p.buildings.mine * 2;
-            p.resources.food += p.buildings.farm * 2;
-            socket.emit('updateResources', p.resources);
-        } else {
-            clearInterval(gameLoop);
-        }
-    }, 2000);
-
-    // Logique de construction
-    socket.on('build', (type) => {
-        let p = players[socket.id];
-        const costs = { mine: 50, farm: 50, barracks: 150 };
-        if (p.resources.gold >= costs[type]) {
-            p.resources.gold -= costs[type];
-            p.buildings[type]++;
-            socket.emit('updateResources', p.resources);
-            socket.emit('msg', `Construction réussie : ${type} niveau ${p.buildings[type]}`);
-        } else {
-            socket.emit('msg', "Or insuffisant !");
-        }
+    socket.on('join', (userData) => {
+        players[socket.id] = {
+            id: socket.id,
+            pseudo: userData.pseudo || "Nomade",
+            x: 0, y: 0, z: 0,
+            hp: 100, food: 100, gold: userData.gold || 100,
+            color: Math.floor(Math.random() * 0xffffff)
+        };
+        socket.emit('init', players[socket.id]);
+        io.emit('currentPlayers', players);
     });
 
     socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            players[socket.id].z = data.z;
-            players[socket.id].ry = data.ry;
+            Object.assign(players[socket.id], data);
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -70,5 +33,4 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Nexus Dynasty lancé sur le port ${PORT}`));
+http.listen(3000, () => console.log("Serveur Nexus Voyage lancé !"));
