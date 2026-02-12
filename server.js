5d1bcd7c7b74c657cfb8c1e382dcb5620fef2084
Gilgamer24 <gilgouper247@gmail.com>
2026-02-12 16:14:05 +0100

    Enhance player connection and movement handling
    Updated player properties and connection handling in Socket.io.

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(__dirname));
-
-let players = {};
-
+// Dans ton fichier server.js (partie Socket.io)
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 0, z: 0, id: socket.id, color: Math.random() * 0xffffff };
+    console.log('Un joueur est connecté : ' + socket.id);
+    
+    // Création du joueur avec une couleur aléatoire
+    players[socket.id] = {
+        x: 0, y: 0, z: 0, ry: 0,
+        id: socket.id,
+        color: Math.floor(Math.random() * 0xFFFFFF)
+    };
+
+    // Envoyer la liste actuelle à celui qui vient de se connecter
     socket.emit('currentPlayers', players);
+
+    // Prévenir les autres qu'un nouveau arrive
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    socket.on('playerMovement', (data) => {
+    // Relayer les mouvements à TOUT LE MONDE
+    socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].z = data.z;
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
+            players[socket.id].z = movementData.z;
+            players[socket.id].ry = movementData.ry;
+            // On renvoie l'info aux autres
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
@@ -27,5 +32,3 @@ io.on('connection', (socket) => {
         io.emit('playerDisconnected', socket.id);
     });
 });
-
-server.listen(process.env.PORT || 10000);
