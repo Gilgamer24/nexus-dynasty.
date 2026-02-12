ef02ccc8551f79f1a07f948ac1debdd6249b8fba
Gilgamer24 <gilgouper247@gmail.com>
2026-02-12 20:30:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,39 +5,40 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-// Base de données en mémoire (pour une persistance après redémarrage serveur, il faudrait un fichier JSON ou MongoDB)
-let db = { 
-    users: {} 
-};
+let players = {}; 
+let db = { users: {} };
 
 io.on('connection', (socket) => {
     socket.on('login', (data) => {
         const { pseudo } = data;
         if (!db.users[pseudo]) {
             db.users[pseudo] = { 
-                pseudo, gold: 50, food: 100, hp: 100, oreStock: 0,
-                houses: [] // Liste des positions {x, z}
+                pseudo, gold: 100, food: 100, hp: 100, oreStock: 0, houses: [] 
             };
         }
         socket.userId = pseudo;
-        socket.emit('authSuccess', db.users[pseudo]);
+        players[socket.id] = { id: socket.id, pseudo: pseudo, x: 0, z: 0 };
+        
+        socket.emit('authSuccess', { me: db.users[pseudo], allPlayers: players });
+        socket.broadcast.emit('playerJoined', players[socket.id]);
     });
 
-    // Sauvegarde les stats et les maisons
+    socket.on('move', (pos) => {
+        if (players[socket.id]) {
+            players[socket.id].x = pos.x;
+            players[socket.id].z = pos.z;
+            socket.broadcast.emit('playerMoved', players[socket.id]);
+        }
+    });
+
     socket.on('saveAll', (data) => {
-        if(socket.userId) {
-            db.users[socket.userId] = data;
-        }
+        if(socket.userId) db.users[socket.userId] = data;
     });
 
-    // Gestion du respawn (Reset stats mais garde les maisons)
-    socket.on('respawnRequest', () => {
-        if(socket.userId) {
-            let u = db.users[socket.userId];
-            u.hp = 100; u.food = 100; u.gold = 0; u.oreStock = 0;
-            socket.emit('authSuccess', u);
-        }
+    socket.on('disconnect', () => {
+        io.emit('playerLeft', socket.id);
+        delete players[socket.id];
     });
 });
 
-http.listen(3000, () => console.log("Nexus Server v8 - Persistance Active"));
+http.listen(3000, () => console.log("Nexus Multi-Engine Online"));
