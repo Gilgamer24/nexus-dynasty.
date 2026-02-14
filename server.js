050eabf9a2e68a2f81c9915b7c4e71c4e6259870
Gilgamer24 <gilgouper247@gmail.com>
2026-02-14 12:19:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -129,3 +129,43 @@ http.listen(PORT, () => {
     ===========================================
     `);
 });
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static(__dirname));
+const DB_PATH = path.join(__dirname, 'database.json');
+let db = { users: {}, globalHouses: [] };
+
+if (fs.existsSync(DB_PATH)) {
+    try { db = JSON.parse(fs.readFileSync(DB_PATH)); } catch (e) {}
+}
+
+function save() { fs.writeFileSync(DB_PATH, JSON.stringify(db, null, 2)); }
+
+let players = {};
+
+io.on('connection', (socket) => {
+    socket.on('login', (data) => {
+        const pseudo = data.pseudo || "Joueur";
+        if (!db.users[pseudo]) {
+            const plotID = (Object.keys(db.users).length % 8) + 1;
+            db.users[pseudo] = { pseudo, gold: 500, wood: 0, plotID, privateHouses: [] };
+            save();
+        }
+        socket.userId = pseudo;
+        players[socket.id] = { id: socket.id, pseudo, x: 0, z: 0, plotID: db.users[pseudo].plotID, zone: 'lobby' };
+        socket.emit('initData', { me: db.users[pseudo], players });
+    });
+
+    socket.on('updatePlayer', (data) => {
+        if(socket.userId) { db.users[socket.userId] = data; save(); }
+    });
+
+    socket.on('disconnect', () => { delete players[socket.id]; });
+});
+
+http.listen(3000, () => console.log("Serveur Multi-Zones OK"));
